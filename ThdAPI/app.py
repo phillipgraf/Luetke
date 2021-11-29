4acbdf633b85cfe907979f4db906231ed35c6f30
Phillip Graf <75568847+HuckRobin@users.noreply.github.com>
2021-11-29 23:18:26 +0100

    Api Calls verbessert

--- ThdAPI/app.py
+++ ThdAPI/app.py
@@ -36,7 +36,7 @@ if fields:
 
 @app.get("/degrees/<degree>") # Returns majors for degree 
 def get_degree_majors(degree):
-    return jsonify([x["name"] for x in majors if escape(degree) in x["degree"]])
+    return jsonify([x["name"] for x in majors if escape(degree).lower() in [c.lower() for c in x["degree"]]])
 @app.get("/majors")
 def get_majors():
     return jsonify([x["name"] for x in majors])
@@ -46,39 +46,39 @@ def major_exists(major):
 @app.get("/fields/exists/<field>")
 def field_exists(field):
     return {"exists": True} if escape(field) in [x["name"] for x in fields] else {"exists": False}
-@app.get("/majors/<major>") # Returns full major information 
+@app.get("/majors/<major>") # Returns full major information
 def get_major(major):
     for m in majors:
-        if m["name"] == escape(major):
+        if m["name"].lower() == escape(major).lower():
             return jsonify(m)
     return jsonify({"name": "Kein Studiengang mit diesem Namen gefunden!"})
-@app.get("/majors/<major>/categories") # Returns available categories from major 
+@app.get("/majors/<major>/categories") # Returns available categories from major
 def get_categories(major):
     for m in majors:
-        if m["name"] == escape(major):
+        if m["name"].lower() == escape(major).lower():
             return jsonify(list(m.keys()))
     return jsonify({"name": "Kein Studiengang mit diesem Namen gefunden!"})
-@app.get("/majors/<major>/<category>") # Returns a specific piece of information from major 
+@app.get("/majors/<major>/<category>") # Returns a specific piece of information from major
 def get_major_category(major, category):
     for m in majors:
-        if m["name"] == escape(major):
+        if m["name"].lower() == escape(major).lower():
             try:
-                return jsonify(m[escape(category)])
+                return jsonify(m[escape(category).lower()])
             except KeyError:
                 return jsonify({"info": "not found"})
     return jsonify({"name": "Kein Studiengang mit diesem Namen gefunden!"})
-@app.get("/fields") # Returns a list of Fields 
+@app.get("/fields") # Returns a list of Fields
 def get_fields():
     return jsonify([x["name"] for x in fields])
-@app.get("/fields/<field>/majors") # Returns all majors concerning a field 
+@app.get("/fields/<field>/majors") # Returns all majors concerning a field
 def get_field_majors(field):
     for f in fields:
-        if f["name"] == escape(field):
+        if f["name"].lower() == escape(field).lower():
             return jsonify(f["majors"])
     return jsonify({"name": "Kein Studienfeld mit diesem Namen gefunden!"})
-@app.get("/fields/<field>/majors/<degree>") # Returns all majors concerning a field 
+@app.get("/fields/<field>/majors/<degree>") # Returns all majors concerning a field
 def get_field_majors_degrees(field, degree):
     for f in fields:
-        if f["name"] == escape(field):
-            return jsonify(f["majors"][0][f"{escape(degree)}."])
+        if f["name"].lower() == escape(field).lower():
+            return jsonify(f["majors"][0][f"{escape(degree).lower()}"])
     return jsonify({"name": "Kein Studienfeld mit diesem Namen gefunden!"})
